9469f792f0895b51564bcd8f6a3ee5e46b79e125
Pivotal <pivotal@Pivotals-iMac-38.local>
2018-05-14 12:43:48 -0700

    yml file added

--- src/main/java/io.pivotal.pal.tracker/WelcomeController.java
+++ src/main/java/io.pivotal.pal.tracker/WelcomeController.java
@@ -2,12 +2,17 @@ package io.pivotal.pal.tracker;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 
 @RestController
 public class WelcomeController {
 
-    @Value("${WELCOME_MESSAGE}")
-    private String message;
+    private final String message;
+
+    public WelcomeController(@Value("${WELCOME_MESSAGE}") String message){
+     this.message = message;
+    }
+
 
     @GetMapping("/")
     public String sayHello() {
